9d68bd7fd2c7740efc213188079f6a9ea174f938
H.K. Brandsma <kewin@frontend-freelance.com>
2023-11-19 18:54:05 +0100

    wip

--- libs/shared/src/brokers/alphavantage/alphavantage.broker.ts
+++ libs/shared/src/brokers/alphavantage/alphavantage.broker.ts
@@ -3,7 +3,8 @@ import { Broker } from '../broker'
 import { OrderResponseACK, OrderResponseFull, OrderResponseResult } from 'binance'
 import alphavantage from 'alphavantage'
 import MOCK_CALENDAR_ITEMS from './calendar.json'
-import { normalizeCalendarData } from './alphavantage.util'
+import { normalizeCalendarData, parseCSV } from './alphavantage.util'
+import axios from 'axios'
 
 export class BrokerAlphavantage extends Broker {
   id = 'Alphavantage'
@@ -15,19 +16,21 @@ export class BrokerAlphavantage extends Broker {
     this.instance = alphavantage({ key: apiKey })
   }
 
-  async getCalendarItems(): Promise<ICalendarItem[]> {
+  async getCalendarItems(mock = true): Promise<ICalendarItem[]> {
     const { apiKey } = this.system.configManager.config.thirdParty.alphavantage
 
-    // const { data } = await axios.get(
-    //   `https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey=${apiKey}`,
-    // )
-    // const items = await parseCSV(data)
-    // writeFileSync('./data3.json', JSON.stringify(items, null, 2))
+    let items: any[] = []
+    if (this.system.configManager.config.dev) {
+      items = MOCK_CALENDAR_ITEMS as any
+    } else {
+      const { data } = await axios.get(`https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey=${apiKey}`)
+      const items = await parseCSV(data)
+    }
 
-    const items = MOCK_CALENDAR_ITEMS as any
+    // writeFileSync('./data3.json', JSON.stringify(items, null, 2))
     return normalizeCalendarData(items)
   }
-  
+
   override syncAccount(): Promise<void> {
     throw new Error('Method not implemented.')
   }
@@ -42,5 +45,5 @@ export class BrokerAlphavantage extends Broker {
   }
   override placeOrder(order: IOrder): Promise<OrderResponseACK | OrderResponseResult | OrderResponseFull> {
     throw new Error('Method not implemented.')
-  }  
+  }
 }
